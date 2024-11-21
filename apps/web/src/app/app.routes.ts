f88139d3427812abb14156c209b8fd776a8d640d
Ion Cretu <cretuion94@gmail.com>
2024-11-21 20:50:17 +0200

    Development (#3)
    * add styles and components for todos and checkboxes; remove unused files
    
    * refactor: fix spells;
    
    * feat: implement view and create/edit todo components with styling
    
    * fix: update CI workflow to use 'main' branch instead of 'master'
    
    * fix: update CI workflow to trigger on 'master' branch
    
    * fix: remove pull request trigger for 'master' branch in CI workflow
    
    * fix: update CI workflow to include pull request trigger for 'master' branch and refactor test file formatting
    
    * refactor: standardize code formatting and remove unused configuration files

--- apps/web/src/app/app.routes.ts
+++ apps/web/src/app/app.routes.ts
@@ -1,13 +1,21 @@
 import { Routes } from "@angular/router"
 import { LayoutComponent } from "./components/layout/layout.component"
+import { checkboxesRoutes } from "./pages/checkboxes/checkboxes.routes"
 import { HomeComponent } from "./pages/home/home.component"
 import { todosRoutes } from "./pages/todos/todos.routes"
-import { checkboxesRoutes } from "./pages/checkboxes/checkboxes.routes"
 
 export const routes: Routes = [
 	{
 		path: "",
 		component: LayoutComponent,
-		children: [{ path: "", component: HomeComponent, pathMatch: "full" }, ...todosRoutes, ...checkboxesRoutes]
+		children: [
+			{
+				path: "",
+				component: HomeComponent,
+				pathMatch: "full"
+			},
+			...todosRoutes,
+			...checkboxesRoutes
+		]
 	}
 ]
